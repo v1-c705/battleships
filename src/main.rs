f5505da9c3f8e88b28de0dbb970c7b3d0d4ed794
v1-c705 <victorwiese@outlook.com>
2024-12-20 16:28:46 +0100

    created an indexed grid

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use iced::{widget::{button, column, container, row, text}, Element, Length::Fill, Theme};
-use rand::{self, seq::SliceRandom};
+use iced::{widget::{button, column, container, row, text, Column}, Element, Length::Fill, Theme};
+//use rand::{self, seq::SliceRandom};
 
 pub fn main() -> iced::Result{
     iced::application("test", update, view)
@@ -7,97 +7,79 @@ pub fn main() -> iced::Result{
     .run()
 }
 
-struct AnArray{
-    array: [[u8; 10]; 10],
-}
+// struct AnArray{
+//     array: [[u8; 10]; 10],
+// }
 
-fn array_2d() -> String {
-    let mut seed = rand::thread_rng();
+// fn array_2d() -> String {
+//     let mut seed = rand::thread_rng();
 
-    let chance : [u8;3]= [0,0,1];
+//     let chance : [u8;3]= [0,0,1];
 
-    let array = [[0 as u8; 10];10];
-    let mut string_array = String::new();
-    let mut array0 = AnArray{
-        array: array,
-    };
+//     let array = [[0 as u8; 10];10];
+//     let mut string_array = String::new();
+//     let mut array0 = AnArray{
+//         array
+//     };
 
 
-    for (row_iter, row) in array.iter().enumerate(){
-        for (column_iter, mut collumn) in row.iter().enumerate(){
-            collumn = chance.choose(&mut seed).unwrap();
-            print!("{}", collumn);
-            if column_iter % 10 == 0 {
-                string_array.push_str("\n");
-                string_array.push_str(collumn.to_string().as_str());
-            } else {
-                string_array.push_str(collumn.to_string().as_str());
-            }
-            array0.array[row_iter][column_iter] = *collumn;
-        }
-        println!();
-    }
-    string_array
-}
+//     for (row_iter, row) in array.iter().enumerate(){
+//         for (column_iter, mut collumn) in row.iter().enumerate(){
+//             collumn = chance.choose(&mut seed).unwrap();
+//             print!("{}", collumn);
+//             if column_iter % 10 == 0 {
+//                 string_array.push_str("\n");
+//                 string_array.push_str(collumn.to_string().as_str());
+//             } else {
+//                 string_array.push_str(collumn.to_string().as_str());
+//             }
+//             array0.array[row_iter][column_iter] = *collumn;
+//         }
+//         println!();
+//     }
+//     string_array
+// }
 
 #[derive(Debug, Clone)]
-enum Message{
-    Show,
+enum Actions{
+    //Show,
+    Label(String),
 }
 
-fn update(value: &mut String, message: Message){
-    match message{
-        Message::Show => value.push_str(&array_2d())
+fn update(value: &mut String, actions: Actions){
+    match actions{
+        //Actions::Show => value.push_str("Show"),
+        Actions::Label(label) => *value = label
     }
 }
 
-fn view(value: &String) -> Element<Message> {
+fn view(value: &String) -> Element<Actions> {
+    let grid: Column<Actions> = column(
+        ('a'..='j').map(|row_index| {
+            row(
+                (1..=10).map(|col_index| {
+                    let label = format!("{}-{}", row_index, col_index);
+                    button("\u{200b}")
+                        .width(32)
+                        .height(32)
+                        .on_press(Actions::Label(label.clone()))
+                        .into()
+                })
+            )
+            .spacing(2)
+            .into()
+        })
+    )
+    .spacing(2);
+
     container(
         column![
-            row![
-                button(text("0")).on_press(Message::Show),
-                button(text("0")).on_press(Message::Show),
-                button(text("0")).on_press(Message::Show),
-                button(text("0")).on_press(Message::Show)
-            ]
-            .spacing(2),
-
-            button(text("0")).on_press(Message::Show),
-            button(text("0")).on_press(Message::Show),
-            button(text("0")).on_press(Message::Show),
-            button(text("0")).on_press(Message::Show),
-
+            grid,
             text(value),
-
         ]
         .spacing(2)
-        .padding(64)
+        .padding(64),
     )
     .center(Fill)
     .into()
-    
 }
-
-// use iced::widget::{button, column, text, Column};
-
-// pub fn main() -> iced::Result {
-//     iced::run("A counter", update, view)
-// }
-
-// #[derive(Debug, Clone)]
-// enum Message {
-//     Increment,
-// }
-
-// fn update(value: &mut u64, message: Message) {
-//     match message {
-//         Message::Increment => *value += 1,
-//     }
-// }
-
-// fn view(value: &u64) -> Column<Message> {
-//     column![
-//         text(value),
-//         button("+").on_press(Message::Increment),
-//     ]
-// }
